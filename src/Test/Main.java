b8f747b2a0373d9ee3af7cc13a5e43297e6fe582
HP <HP@192.168.1.78>
2025-07-04 18:15:41 +0530

    CHANGES

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -7,16 +7,16 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Calculator c = new CalculatorImpl();
-		int sum = c.sum(12, 34);
+		int sum = c.sum(25, 34);
 		System.out.println(sum);
 		
-		int div = c.div(10, 2);
+		int div = c.div(25, 2);
 		System.out.println(div);
 		
-		int sub = c.sub(28, 8);
+		int sub = c.sub(25, 8);
 		System.out.println(sub);
 		
-		int multi=c.mult(20, 10);
+		int multi=c.mult(28, 10);
 		System.out.println(multi);
 		
 	}
